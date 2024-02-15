ad78500047d2f80795ec2b04c57c8d66c4f27cde
Keri <asteria@Keris-MacBook-Pro-2.local>
2024-02-15 14:59:17 -0800

    0215 updates

--- components/features.tsx
+++ components/features.tsx
@@ -6,7 +6,7 @@ export default function Features() {
 
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
-            <h2 className="h2 mb-4">Our Clients.</h2>
+            <h2 className="h2 mb-4">Our Clients</h2>
             <p className="text-xl text-gray-400">We work with buildings all over New York City. <br/>Our services are available at the following locations:</p>
           </div>
 
